a50e13d162d1798596e6892e730503863cdc1187
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-04-23 18:54:27 +0300

    Add files via upload

---
+++ Source Code/Dom5_Trainer.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Dominions5WOTFTrainer
+{
+    partial class Dom5_Trainer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 184);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(219, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Dominions 5 WOTF Trainer";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei Light", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Aqua;
+            this.button1.Location = new System.Drawing.Point(12, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "About";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(0, 289);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(805, 173);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(671, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 53);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(323, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(191, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Infinite Astral Perls";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Dom5_Trainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Dom5_Trainer";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dominions 5 WOTF Trainer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Source Code/Dom5_Trainer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memory;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Dominions5WOTFTrainer
+{
+    public partial class Dom5_Trainer : Form
+    {
+        public Mem dom5 = new Mem();
+        public Dom5_Trainer()
+        {
+            InitializeComponent();
+            dom5.GetThreads();
+            dom5.OpenProcess(dom5.GetProcIdFromName("Dominions5"));
+            if (dom5.OpenProcess(dom5.GetProcIdFromName("Dominions5")))
+            {
+                Trace.WriteLine("Dominions 5 is Found");
+            }
+            else
+            {
+                Trace.WriteLine("Dominions 5 is not Found");
+                MessageBox.Show("Please Run Dominions 5 and try again run Trainer");
+                Environment.Exit(0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = "Thank u for Choosing this Trainer, Made by BuzzerFelix";
+            MessageBox.Show(text, "Dominions 5 WOTF Trainer", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string dom5astral = "Dominions5.exe+1A67664";
+            string astr = "Dominions5.exe+25D73DA";
+            string dom5infastr = "20000";
+            dom5.WriteMemory(dom5astral, "int", dom5infastr);
+            dom5.WriteMemory(astr, "int", dom5infastr);
+            dom5.ReadInt(dom5astral);
+            dom5.ReadInt(astr);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dom5.CloseProcess();
+            Environment.Exit(3432);
+        }
+    }
+}
